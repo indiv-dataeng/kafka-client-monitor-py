8dd773b051347fdd2fe36f87f658d8d30cb27242
Pon-ek Tangmunchittham <pon-ek@hotmail.com>
2022-02-14 08:13:22 +0700

    Setup Kafka cluster (Docker), Producer & Consumer (Java)

---
+++ kafka-producer/src/test/java/com/indiv/kafka/producer/AppTest.java
@@ -0,0 +1,20 @@
+package com.indiv.kafka.producer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
